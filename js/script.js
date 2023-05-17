19ee12ee0879248f5ee3051b9e66d159722436da
Gabriel Marques <gabrielmqs1415@gmail.com>
2023-05-17 15:27:57 -0300

    17 de maio de 2023

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function soma(n1, n2) {
+    alert(n1 + n2)
+}
+function subtracao(n1, n2) {
+    alert(n1 - n2)
+}
+function multiplicacao(n1, n2) {
+    alert(n1 * n2)
+}
+function divisao(n1, n2) {
+    alert(n1 / n2)
+}
+
+function mostrarnome() {
+    let nome = prompt("Qual é seu nome?")
+    alert("Olá, " + nome)
+}
+function areadoretangulo() {
+    let base = prompt("Qual é a base")
+    let altura = prompt("Qual é a altura")
+    let area
+    area = base * altura
+    alert(area)
+}
+function areadocirculo() {
+    let pi = 3,14
+    let raio = prompt("Qual é a altura")
+    let area
+    area = pi * raio * raio
+    alert(area)
+}
